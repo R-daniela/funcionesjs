f7a890465238cc91c58876743e8b44f23be2c55d
Rocio Daniela Montero <rociodanielamonteroc@gmil.com>
2022-09-16 09:12:27 -0500

    funciones

--- 123/kk.js
+++ 123/kk.js
@@ -1,7 +1,29 @@
-for(let prop of person){
+/* for(let prop of person){
     console.log(prop)
-}
+} */
 const a=1
 const b=2
-let suma =a+b 
+
+function sumar(){
+    let suma=a+b 
+    console.log(suma)
+}
+sumar()
 console.log(suma)
+
+/* funcion expresada */
+
+
+const sumar1 = function(a,b){
+    let suma = a+b 
+    console.log(suma)
+}
+sumar(1,2)
+
+/* arrow function */
+
+const sumar3 = (a+b)=>{
+    let suma  = a+b 
+    console.log(suma)
+}
+sumar3(4,6)
