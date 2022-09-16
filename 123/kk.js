7f7b5c1cf228dcb785b668f201f07b4add120eb5
Rocio Daniela Montero <rociodanielamonteroc@gmil.com>
2022-09-16 09:38:52 -0500

    funcion multiplicar

--- 123/kk.js
+++ 123/kk.js
@@ -1,7 +1,7 @@
 /* for(let prop of person){
     console.log(prop)
 } */
-const a=1
+/* const a=1
 const b=2
 
 function sumar(){
@@ -13,7 +13,7 @@ console.log(suma)
 
 /* funcion expresada */
 
-
+/* 
 const sumar1 = function(a,b){
     let suma = a+b 
     console.log(suma)
@@ -22,8 +22,20 @@ sumar(1,2)
 
 /* arrow function */
 
-const sumar3 = (a+b)=>{
+/* const sumar3 = (a+b)=>{
     let suma  = a+b 
     console.log(suma)
 }
-sumar3(4,6)
+sumar3(4,6)  */ 
+
+
+
+ let number =+prompt("Giv me a number")
+
+ function multiplicate(number){
+    for( let i=1;i<=9;i++){
+        document.write(`${number} x ${i} = ${number*i}<br>`)
+    }
+ }
+
+ multiplicate(number)
